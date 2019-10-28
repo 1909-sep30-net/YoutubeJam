420ed945c718a3318b92978bacbd860309e71fb9
Angad <angadsinghminhas@hotmail.com>
2019-10-28 11:29:44 -0500

    Sentiment Analysis Test Implementation

---
+++ YoutubeJam.Auth/AzureTextAnalyticsAuth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeJam.Auth
+{
+    public class AzureTextAnalyticsAuth
+    {
+        private readonly string apiKey = "be6e6df747a54718939279d9a26640ef";
+        private readonly string endPoint = "https://youtubejam.cognitiveservices.azure.com/";
+        private object subscriptionKey;
+
+        //public AzureTextAnalyticsAuth(string apiKey, string endPoint)
+        //{
+        //    this.apiKey = apiKey;
+        //    this.endPoint = endPoint;
+        //}
+
+        public string GetAPIKey()
+        {
+            return apiKey;
+        }
+        public string GetEndPoint()
+        {
+            return endPoint;
+        }
+
+    }
+}
---
+++ YoutubeJam.BusinessLogic/SentimentAnalysis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Rest;
+using System.Net.Http;
+using System.Web;
+using YoutubeJam.Auth;
+
+namespace YoutubeJam.BusinessLogic
+{
+    
+    static class SentimentAnalysis
+    {
+
+
+        public static void AuthenticateAnalysis()
+        {
+            var credentials = new AzureTextAnalyticsAuth();
+            var credentialsX = new ApiKeyServiceClientCredentials(subscriptionKey);
+
+
+            string apiKey = credentials.GetAPIKey();
+            string endpoint = credentials.GetEndPoint();
+
+            TextAnalyticsClient client = new TextAnalyticsClient()
+            {
+                Endpoint = endpoint
+            };
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            SentimentAnalysisExample(client);
+            // languageDetectionExample(client);
+            // entityRecognitionExample(client);
+            // KeyPhraseExtractionExample(client);
+        }
+
+
+        static void SentimentAnalysisExample(TextAnalyticsClient client)
+        {
+            var result = client.Sentiment("I had the best day of my life.", "en");
+            Console.WriteLine($"Sentiment Score: {result.Score:0.00}");
+        }
+
+    }
+   
+}	
