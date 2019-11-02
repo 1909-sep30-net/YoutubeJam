c3673c6916a54edc2e26077b355a6b28f1d220ff
Angad <angadsinghminhas@hotmail.com>
2019-11-02 16:41:08 -0500

    Added Video List Retrieval For User Sign Up

--- YoutubeJam.Api/Startup.cs
+++ YoutubeJam.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Linq;
 using YoutubeJam.Auth;
 using YoutubeJam.BusinessLogic;
 using YoutubeJam.Persistence;
@@ -32,6 +33,7 @@ namespace YoutubeJam.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "YoutubeJam API", Version = "v1" });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
             });
 
 
